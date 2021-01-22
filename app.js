4f133e48de64bb9c236d0d140de5a750ee256f52
AnisBouchrika <anice.bouchrika@gmail.com>
2021-01-22 13:43:25 +0100

    app

--- app.js
+++ app.js
@@ -41,9 +41,10 @@ app.use((req, res, next) => {
 
 app.use('/api/articles', articles);
 app.use('/api/users', users);
+app.use('/', express.static(path.join(__dirname, '/client/build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+    res.sendFile(path.join(__dirname, "client","build","index.html"));
 });
 
 app.listen(PORT, () => {
